8f95838fee3fdb70f0df3d7041536154170bee62
Arturo Caronongan <arturo.caronongan@delasalle.ph>
2026-03-23 10:36:06 +0800

    sample render

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express'); // We will make use of the express API
+var app = express();
+
+var bodyParser = require('body-parser'); // Included in express already
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req,res)
+{
+    res.sendFile(__dirname + '\\' + 'index.html');
+});
+
+app.post('/submit-student-data', function(req,res)
+{
+    var name = req.body.firstName + ' ' + req.body.lastName;
+
+    res.send(name + ' Submitted Successfully!');
+});
+
+app.get('/submit-student-data', function(req,res)
+{
+    res.send ("Invalid acccess attempted");
+});
+
+var server = app.listen(5000, function()
+{
+    console.log("Listening at port 5000...");
+});
